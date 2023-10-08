a3091ed524196eced8eef077a6012008860fffd8
Aldo Santiago Alejandro <26aldo.santiago@gmail.com>
2023-10-08 01:11:31 -0600

    Se agregan js del Espadas

---
+++ js/Jugador.js
@@ -0,0 +1,15 @@
+
+class Jugador {
+    constructor(id_j) {
+        this.id_j = id_j;
+        this.posicion = 0;
+    }
+
+    /** Método */
+    tirarDado() { // Usa la sintaxis de función tradicional para definir métodos
+        return Math.ceil(Math.random() * 6);
+    }
+}
+
+const jugador = new Jugador(1);
+console.log(jugador.tirarDado());
